b31121e80a4e3022956c928257076e220866a0d2
satishccy <satishchoudhari13579@gmail.com>
2024-09-10 14:46:07 +0530

    added 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import { Challenge2Page } from './components/Challenge2/Challenge2.page';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/ctf-2024/dist">
         <nav>
             <ul>
-                <li><Link to="./challenge-1">Challenge 1</Link></li>
-                <li><Link to="./challenge-2">Challenge 2</Link></li>
+                <li><Link to="/challenge-1">Challenge 1</Link></li>
+                <li><Link to="/challenge-2">Challenge 2</Link></li>
             </ul>
         </nav>
         <main>
             <Routes>
-                <Route path="challenge-1" element={<Challenge1Page />} />
-                <Route path="challenge-2" element={<Challenge2Page />} />
+                <Route path="/challenge-1" element={<Challenge1Page />} />
+                <Route path="/challenge-2" element={<Challenge2Page />} />
             </Routes>
         </main>
     </Router>
