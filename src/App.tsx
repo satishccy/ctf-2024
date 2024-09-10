b037d4f76c689b3aaf2ae498bd88a58b59677fb6
satishccy <satishchoudhari13579@gmail.com>
2024-09-10 14:29:03 +0530

    typo

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,14 @@ function App() {
     <Router basename='ctf-2024/dist/'>
         <nav>
             <ul>
-                <li><Link to="/challenge-1">Challenge 1</Link></li>
-                <li><Link to="/challenge-2">Challenge 2</Link></li>
+                <li><Link to="./challenge-1">Challenge 1</Link></li>
+                <li><Link to="./challenge-2">Challenge 2</Link></li>
             </ul>
         </nav>
         <main>
             <Routes>
-                <Route path="/challenge-1" element={<Challenge1Page />} />
-                <Route path="/challenge-2" element={<Challenge2Page />} />
+                <Route path="./challenge-1" element={<Challenge1Page />} />
+                <Route path="./challenge-2" element={<Challenge2Page />} />
             </Routes>
         </main>
     </Router>
